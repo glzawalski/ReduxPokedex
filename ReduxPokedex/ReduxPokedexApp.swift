c40e9f530276e9a1a6b0f2cfe41c839074a836c4
glzawalski <gabrielzawalski@gmail.com>
2023-02-26 01:01:39 -0300

    Initial Commit

---
+++ ReduxPokedex/ReduxPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxPokedexApp.swift
+//  ReduxPokedex
+//
+//  Created by Gabriel Zawalski on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ReduxPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
